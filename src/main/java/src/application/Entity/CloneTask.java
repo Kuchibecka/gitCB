aca5b4ae236640d7bd49933c59e9b20c83ab9cdb
Антон Зверев <zverev827@gmail.com>
2021-12-03 13:39:06 +0300

    Progress indicator + new todos

---
+++ src/main/java/src/application/Entity/CloneTask.java
@@ -0,0 +1,75 @@
+package src.application.Entity;
+
+import javafx.concurrent.Task;
+import org.apache.logging.log4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class CloneTask extends Task<String> {
+
+    private String cloneCommand;
+
+    private Logger rootLogger;
+
+    public CloneTask(String cloneCommand, Logger rootLogger) {
+        this.cloneCommand = cloneCommand;
+        this.rootLogger = rootLogger;
+    }
+
+    public String getCloneCommand() {
+        return cloneCommand;
+    }
+
+    public void setCloneCommand(String cloneCommand) {
+        this.cloneCommand = cloneCommand;
+    }
+
+    @Override
+    protected String call() {
+        rootLogger.info("Calling git clone task");
+        try {
+            rootLogger.debug("Full command for ProcessBuilder is: " + cloneCommand);
+            ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c", cloneCommand);
+            builder.redirectErrorStream(true);
+            Process p = builder.start();
+            BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line;
+            int totalProgress = 0;
+            int progress = 0;
+            while (true) {
+                line = r.readLine();
+                if (line == null) {
+                    break;
+                }
+                // Вывод командной строки
+                if (line.contains(":") && line.contains("%") && !line.contains("done")) {
+                    int curProgress = Integer.parseInt(line.substring(line.lastIndexOf(':') + 1, line.lastIndexOf('%')).replaceAll("\\s+", ""));
+                    if (curProgress >= progress) {
+                        totalProgress = totalProgress - progress + curProgress;
+                        this.updateProgress(totalProgress, 400);
+                        progress = curProgress;
+                        if (progress == 100)
+                            progress = 0;
+                    }
+                }
+                if (line.contains("Receiving objects: 100%"))
+                    this.updateProgress(400, 400);
+
+                // todo: v вернуть v
+                rootLogger.debug("Trace from ProcessBuilder: " + line);
+                if (line.contains("already exists and is not an empty directory.")) {
+                    rootLogger.error("Directory already exists");
+                    this.updateProgress(0, 400);
+                    return "Непустая папка с таким названием уже существует";
+                }
+            }
+        } catch (IOException e) {
+            rootLogger.error(e.getMessage());
+            return "Ошибка выполнения команды git clone";
+        }
+        return "none";
+    }
+}
--- src/main/java/src/application/RepoController.java
+++ src/main/java/src/application/RepoController.java
@@ -2,23 +2,23 @@ package src.application;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.collections.FXCollections;
+import javafx.concurrent.WorkerStateEvent;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+import src.application.Entity.CloneTask;
 import src.application.Entity.Repository;
 import src.application.Entity.RepositoryContainer;
 
@@ -27,6 +27,9 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class RepoController implements Initializable {
@@ -50,6 +53,13 @@ public class RepoController implements Initializable {
     private TextField pathField;
     @FXML
     private Label pathLabel;
+    @FXML
+    private ProgressBar progressBar = new ProgressBar(0);
+    @FXML
+    private ProgressIndicator progressIndicator = new ProgressIndicator(0);
+    @FXML
+    private Label progressLabel = new Label();
+
 
     static final Logger rootLogger = LogManager.getRootLogger();
 
@@ -59,11 +69,11 @@ public class RepoController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         nameList.getSelectionModel().selectedItemProperty().addListener((arg0, arg1, arg2) -> {
-            curRepoUrl = repositories
-                    .getRepoMap()
-                    .get(nameList.getSelectionModel().getSelectedItem())
-                    .getRepoUrl()
-                    .substring(8);
+            Repository repository = repositories.getRepoMap().get(nameList.getSelectionModel().getSelectedItem());
+            if (repository != null)
+                curRepoUrl = repository
+                        .getRepoUrl()
+                        .substring(8);
             rootLogger.debug("Selected repo with name: " + nameList.getSelectionModel().getSelectedItem() + " and URL: " + curRepoUrl);
         });
     }
@@ -92,11 +102,17 @@ public class RepoController implements Initializable {
         DirectoryChooser dirChooser = new DirectoryChooser();
         Stage stage = new Stage();
         File file = dirChooser.showDialog(stage);
-        if (file != null) {
+        if (file != null && Files.exists(Paths.get(file.getAbsolutePath()))) {
             this.curDirectory = file.getAbsolutePath();
             pathLabel.setText(this.curDirectory);
+        } else {
+            dirChooser.setInitialDirectory(new File("C:\\"));
+            this.curDirectory = "C:\\";
+            pathLabel.setText(this.curDirectory);
+            errorLabel2.setText("Выбранной директории не существует");
+            rootLogger.error("No such directory");
         }
-        System.out.println("Picked directory: " + this.curDirectory);
+        rootLogger.debug("Picked directory: " + this.curDirectory);
     }
 
     /**
@@ -137,13 +153,9 @@ public class RepoController implements Initializable {
      */
     public HashMap<String, Repository> request(String url) throws Exception {
         rootLogger.info("Calling request(" + url + ")");
-        /*try {*/
         HashMap<String, Repository> repoMap = new HashMap<>();
-
         RestTemplate restTemplate = new RestTemplate();
-        System.out.println(url);
-        String json = null;
-        json = restTemplate.getForObject(url, String.class);
+        String json = restTemplate.getForObject(url, String.class);
         if (json == null || json.isEmpty()) {
             throw new Exception("Protocol not specified");
         }
@@ -191,7 +203,7 @@ public class RepoController implements Initializable {
     public void cloneRepo() {
         errorLabel2.setText("");
         rootLogger.info("Calling cloneRepo()");
-        String command = "git clone https://gitlab-ci-token:"
+        String command = "git clone --progress https://gitlab-ci-token:"
                 + this.repositories.getToken() + "@" + curRepoUrl;
         rootLogger.debug("Command for ProcessBuilder is: " + command);
         if (curRepoUrl == null || curRepoUrl.isEmpty()) {
@@ -204,7 +216,30 @@ public class RepoController implements Initializable {
             errorLabel2.setText("Путь клонирования не указан");
             return;
         }
-        try {
+        String pbCommand = "cd " + curDirectory + " && " + command;
+        CloneTask cloneTask = new CloneTask(pbCommand, rootLogger);
+        progressBar.progressProperty().unbind();
+        progressBar.progressProperty().bind(cloneTask.progressProperty());
+        progressIndicator.progressProperty().unbind();
+        progressIndicator.progressProperty().bind(cloneTask.progressProperty());
+        progressLabel.textProperty().unbind();
+        progressLabel.textProperty().bind(cloneTask.messageProperty());
+
+        cloneTask.addEventHandler(WorkerStateEvent.WORKER_STATE_SUCCEEDED,
+                event -> {
+                    String result = cloneTask.getValue();
+                    progressLabel.textProperty().unbind();
+                    if (result.equals("none")) {
+                        progressLabel.setText("Готово");
+                    } else {
+                        errorLabel2.setText(result);
+                        progressLabel.setText("Что-то пошло не так");
+
+                    }
+                });
+
+        new Thread(cloneTask).start();
+        /*try {
             rootLogger.debug("Path for ProcessBuilder is: " + curDirectory);
             String pbCommand = "cd " + curDirectory + " && " + command;
             rootLogger.debug("Full command for ProcessBuilder is: " + pbCommand);
@@ -213,28 +248,38 @@ public class RepoController implements Initializable {
             Process p = builder.start();
             BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
             String line;
+            *//*int count = 400;*//*
+            int totalProgress = 0;
+            int progress = 0;
             while (true) {
                 line = r.readLine();
                 if (line == null) {
                     break;
                 }
                 // Вывод командной строки
+                if (line.contains(":") && line.contains("%") && !line.contains("done")) {
+                    int curProgress = Integer.parseInt(line.substring(line.lastIndexOf(':') + 1, line.lastIndexOf('%')).replaceAll("\\s+", ""));
+                    System.out.println(curProgress);
+                    if (curProgress > progress *//*|| curProgress == 0*//*) {
+                        totalProgress = totalProgress - progress + curProgress;
+                        System.out.println("************Total progress is: " + totalProgress);
+                        progress = curProgress;
+                        if (progress == 100)
+                            progress = 0;
+                    }
+                }
                 rootLogger.debug("Trace from ProcessBuilder: " + line);
                 if (line.contains("already exists and is not an empty directory.")) {
                     rootLogger.error("Directory already exists");
                     errorLabel2.setText("Непустая папка с таким названием уже существует");
                 }
-                if (line.contains("Receiving objects: 100%")) {
-                    // System.out.println("СТООООООООООООООООООООООООООООООООООООООООООООО");
-                    /*rootLogger.error("Directory already exists");
-                    errorLabel2.setText("Непустая папка с таким названием уже существует");*/
-                }
+                // todo: обновление статуса
             }
         } catch (IOException e) {
             rootLogger.error(e.getMessage());
             errorLabel2.setText("Ошибка выполнения команды git clone");
             return;
-        }
+        }*/
         rootLogger.info("Result of cloneRepo(): executed attempt to clone selected repository");
     }
 }
